1b38a94dae5019173ae690723011091d030d0e3b
tavisca-chaitalishah <cshah@tavisca.com>
2017-07-12 15:24:34 +0530

    Added #

--- learn_chef_apache2/recipes/default.rb
+++ learn_chef_apache2/recipes/default.rb
@@ -1,7 +1,7 @@
 #
 # Cookbook Name:: learn_chef_apache2
 # Recipe:: default
-# ########
+# #########
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 apt_update 'Update the apt cache daily' do
   frequency 86_400
